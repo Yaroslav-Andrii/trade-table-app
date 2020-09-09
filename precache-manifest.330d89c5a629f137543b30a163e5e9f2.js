3d98a02fccfdb8162a618d679400b1742fa0fb75
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-09 04:36:26 +0300

    Updates

---
+++ precache-manifest.330d89c5a629f137543b30a163e5e9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fabb1073a1efb19b8973fc41627d5b2",
+    "url": "/trade-table-app/index.html"
+  },
+  {
+    "revision": "795b722e462f26f6d7fc",
+    "url": "/trade-table-app/static/css/main.0a56487e.chunk.css"
+  },
+  {
+    "revision": "bb951926bf909ca81e86",
+    "url": "/trade-table-app/static/js/2.faa454c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trade-table-app/static/js/2.faa454c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795b722e462f26f6d7fc",
+    "url": "/trade-table-app/static/js/main.715e38dc.chunk.js"
+  },
+  {
+    "revision": "cb481d3d948279ba4648",
+    "url": "/trade-table-app/static/js/runtime-main.ce6c6ef8.js"
+  }
+]);
